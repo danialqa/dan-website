f3c8dcf850de45d44ae93e6644b53c1874173ea8
danialghahremani <dan.ghahremani@gmail.com>
2022-04-15 23:27:35 +0430

    Bug fix

--- pages/home/Home.page.tsx
+++ pages/home/Home.page.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/Image';
+import Image from 'next/image';
 
 import { Helmet } from 'components';
 
@@ -6,25 +6,25 @@ import { Box, Flex, Text } from 'components';
 
 export default function Home() {
   return (
-    <div className="container">
-      <Helmet title="Front-end Developer" />
+    <div className='container'>
+      <Helmet title='Front-end Developer' />
 
-      <Flex color="#fff">
+      <Flex color='#fff'>
         <Image
-          src="/images/me.png"
+          src='/images/me.png'
           width={533}
           height={261}
-          layout="intrinsic"
+          layout='intrinsic'
         />
 
         <Box>
           <Box
-            maxWidth="395px"
-            background="gray1"
-            padding="20px"
-            borderRadius="23px"
+            maxWidth='395px'
+            background='gray1'
+            padding='20px'
+            borderRadius='23px'
           >
-            <Text fontSize="18px">
+            <Text fontSize='18px'>
               I’m a Front-End Developer and aslo a UI/UX designer! based in
               Tehran, IRAN
             </Text>
