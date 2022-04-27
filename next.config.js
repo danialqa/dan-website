79e93480d8404abe1996c0a3bf8eb6ad24907aa1
danialghahremani <dan.ghahremani@gmail.com>
2022-04-27 14:13:37 +0430

    Svg support!

--- next.config.js
+++ next.config.js
@@ -4,6 +4,26 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.tsx?$/,
+      use: [
+        'next-swc-loader',
+        {
+          loader: '@svgr/webpack',
+        },
+      ],
+    });
+
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.dns = false;
+      config.resolve.fallback.net = false;
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
